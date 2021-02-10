f9e41bdf000cc6972a138cb537eeeff8166c4cda
Zac <zac@zacland.net>
2021-02-10 14:59:29 +0100

    [Init]

---
+++ FactorioSync2/Helpers.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+using NLog;
+
+namespace FactorioSync2
+{
+    public class Helpers
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private static readonly string[] _args = Environment.GetCommandLineArgs();
+
+        private enum ErrorLvl
+        {
+            Fatal,
+            Error,
+            Warn,
+            Info,
+            Debug,
+            Trace
+        }
+
+        private static void LogString(string text, ListBox lstBox = null, ErrorLvl error = ErrorLvl.Debug, string color = "")
+        {
+            if ((lstBox != null) && !((_args.Length > 1) && _args[1] == "/silent"))
+            {
+                // On log sur l'interface
+                var defaultBrushColor = Brushes.Blue;
+                var successBrushColor = Brushes.Green;
+                var errorBrushColor = Brushes.Red;
+                var warningBrushColor = Brushes.Orange;
+
+                var currentBrushe = defaultBrushColor;
+
+                if (color == "success")
+                {
+                    currentBrushe = successBrushColor;
+                }
+                else if (color == "error")
+                {
+                    currentBrushe = errorBrushColor;
+                }
+                else if (color == "warning")
+                {
+                    currentBrushe = warningBrushColor;
+                }
+
+                lstBox.Items.Add(new ListBoxItem
+                {
+                    Content = String.Format(text),
+                    Foreground = currentBrushe
+                });
+            }
+            else
+            {
+                // Sinon on log dans le fichier de Log
+                if (error == ErrorLvl.Debug)
+                {
+                    _logger.Debug(text);
+                }
+                else if (error == ErrorLvl.Error)
+                {
+                    _logger.Error(text);
+                }
+                else
+                {
+                    _logger.Info(text);
+                }
+
+            }
+        }
+
+        public static void CopyFile(string srcFolder, string destFolder, ListBox lstBox = null)
+        {
+            string[] originalFiles = Directory.GetFiles(srcFolder, "*", SearchOption.AllDirectories);
+
+            LogString($"Début : {DateTime.Now}", lstBox);
+            LogString($"Il y a {originalFiles.Length} fichiers dans {srcFolder}", lstBox);
+
+            Array.ForEach(originalFiles, (originalFileLocation) =>
+            {
+                try
+                {
+                    FileInfo originalFile = new FileInfo(originalFileLocation);
+                    FileInfo destFile = new FileInfo(originalFileLocation.Replace(srcFolder, destFolder));
+
+                    LogString($"Vérification du fichier {originalFile} vers le répertoire {destFolder}", lstBox);
+
+                    if (destFile.Exists)
+                    {
+                        LogString($"Le fichier {destFile} existe déja !", lstBox);
+                        if (originalFile.LastWriteTimeUtc > destFile.LastWriteTimeUtc)
+                        {
+                            LogString($"Le fichier d'origine \"{originalFile}\" >{originalFile.LastWriteTimeUtc}< est plus récent que le fichier de destination \"{destFile}\" >{destFile.LastWriteTimeUtc}< : On l'écrase", lstBox, ErrorLvl.Info, "succes");
+                            try
+                            {
+                                originalFile.CopyTo(destFile.FullName, true);
+                                LogString($"Copie effectuée : \"{originalFile}\" -> \"{destFile}\" !", lstBox, ErrorLvl.Info, "succes");
+                            }
+                            catch (Exception e)
+                            {
+                                LogString($"Erreur lors de la copie du fichier {originalFile} : {e.Message}", lstBox, ErrorLvl.Error, "error");
+                            }
+                        }
+                        else
+                        {
+                            LogString($"Les fichiers sont identiques", lstBox, ErrorLvl.Info, "succes");
+                        }
+                    }
+                    else
+                    {
+                        LogString($"Création/Sélection du répertoire \"{destFile.DirectoryName}\"", lstBox, ErrorLvl.Debug, "warning");
+                        Directory.CreateDirectory(destFile.DirectoryName);
+                        try
+                        {
+                            LogString($"Copie du fichier {destFile.FullName}", lstBox, ErrorLvl.Debug, "success");
+                            originalFile.CopyTo(destFile.FullName, false);
+                        }
+                        catch (Exception ex)
+                        {
+                            LogString($"Erreur lors de la copie du fichier {destFile.FullName} : {ex.Message}", lstBox, ErrorLvl.Error, "error");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    LogString(e.Message, lstBox, ErrorLvl.Error, "error");
+                }
+            });
+
+            LogString($"Fin : {DateTime.Now}", lstBox);
+            LogString("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*", lstBox);
+        }
+    }
+}
--- FactorioSync2/MainWindow.xaml.cs
+++ FactorioSync2/MainWindow.xaml.cs
@@ -1,28 +1,116 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using FactorioSync2.Annotations;
 
 namespace FactorioSync2
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string _source;
+        private string _destination;
+
+        public string Source
+        {
+            get
+            {
+                return _source;
+            }
+            set
+            {
+                _source = value;
+                OnPropertyChanged(nameof(Source));
+            }
+        }
+
+        public string Destination
+        {
+            get
+            {
+                return _destination;
+            }
+            set
+            {
+                _destination = value;
+                OnPropertyChanged(nameof(Destination));
+            }
+        }
+
+
+        private readonly string[] _args = Environment.GetCommandLineArgs();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            Source = @"C:\Users\C.ROUXEL\AppData\Roaming\Factorio\saves";
+            Destination = @"C:\Users\C.ROUXEL\Dropbox\__FACTORIO_saved_games";
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CheckBtnGo();
+
+            if ((_args.Length > 1) && _args[1] == "/silent")
+            {
+                BtnGo_Click(sender, e);
+            }
+        }
+
+        private bool CheckBtnGo()
+        {
+            bool retour;
+
+            bool isSourceExist = Directory.Exists(Source);
+            bool isDestinationExist = Directory.Exists(Destination);
+
+            BtnGo.IsEnabled = retour = (isSourceExist && isDestinationExist);
+
+            return retour;
+        }
+
+        private void BtnGo_Click(object sender, RoutedEventArgs e)
+        {
+            BtnReset_Click(sender, e);
+            Helpers.CopyFile(Source, Destination, listBox);
+            Helpers.CopyFile(Destination, Source, listBox);
+
+
+            if ((_args.Length > 1) && _args[1] == "/silent" && CheckBtnGo())
+            {
+                this.Close();
+            }
+        }
+
+        private void TxtSource_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckBtnGo();
+        }
+
+        private void TxtDestination_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckBtnGo();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
